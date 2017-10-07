de3dfaf588e184ebb07a2d706d2ecec073fb763e
venkatraina <venkat968824@gmail.com>
2017-10-07 16:17:22 +0530

    Update P62.java

--- P62.java
+++ P62.java
@@ -5,37 +5,38 @@ import java.util.*;
 public class P62 {
 
 	public static void main(String[] args) {
-		Scanner in=new Scanner(System.in);
-		String s=in.next();
+		Scanner in = new Scanner(System.in);
+		String s = in.next();
 		searchString(s);
 		in.close();
 		System.out.println(searchString(s));
 	}
+
 	public static String searchString(String s) {
-		String a="";
-		OUTER:
-		for(int i=s.length();i>1;i--) {
-			for(int j=0;j<=s.length()-i;j++) {
-				a="";
-				for(int k=j;k<j+i;k++) {
-					a+=s.charAt(k)+"";
+		String a = "";
+		OUTER: for (int i = s.length(); i > 1; i--) {
+			for (int j = 0; j <= s.length() - i; j++) {
+				a = "";
+				for (int k = j; k < j + i; k++) {
+					a += s.charAt(k) + "";
 				}
-				if(palindrome(a)) {
+				if (palindrome(a)) {
 					break OUTER;
 				}
 			}
 		}
 		return a;
 	}
+
 	public static boolean palindrome(String s) {
-		StringBuffer sr=new StringBuffer(s);
-		String s1=sr.reverse().toString();
-		if(s1.equals(s)) {
+		StringBuffer sr = new StringBuffer(s);
+		String s1 = sr.reverse().toString();
+		if (s1.equals(s)) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
-		
+
 	}
 
 }
